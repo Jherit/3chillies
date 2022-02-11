813cdede03017f4e21c923c2eaa213ddf0b5aaac
John Heritage <john@johnheritage.com>
2022-02-11 00:03:55 +0000

    images added ad basic gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+
+
+gulp.task('default', () => {
+ return gulp.src('app/src/styles/styles.css')
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+  .pipe(gulp.dest('app/dist/styles'))
+});
